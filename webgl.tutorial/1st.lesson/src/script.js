7cb7e1d48a1c7ba093cfdfbd9d1b4c6ff891a869
Lucas Eduardo Bretana <lebretana@inf.ufpel.edu.br>
2019-10-29 21:39:31 -0300

    WIP(continuing the fundamentals..)

--- webgl.tutorial/1st.lesson/src/script.js
+++ webgl.tutorial/1st.lesson/src/script.js
@@ -1,39 +1,43 @@
-// WebGL2 - Fundamentals
-// from https://webgl2fundamentals.org/webgl/webgl-fundamentals.html
+"use strict";
 
+var vertexShaderSource = "" +
+  "   #version 300 es \n" +
+  "   // an attribute is an input (in) to a vertex shader.\n" +
+  "   // It will receive data from a buffer\n" +
+  "   in vec4 a_position;\n" +
+  "   \n" +
+  "   // all shaders have a main function\n" +
+  "   void main() {\n" +
+  "   \n" +
+  "     // gl_Position is a special variable a vertex shader\n" +
+  "     // is responsible for setting\n" +
+  "     gl_Position = a_position;\n" +
+  "   }\n" +
+"";
 
-  "use strict";
+var fragmentShaderSource = ""+
+  "   #version 300 es\n" +
+  "   \n" +
+  "   // fragment shaders don't have a default precision so we need\n" +
+  "   // to pick one. mediump is a good default. It means \"medium precision\"\n" +
+  "   precision mediump float;\n" +
+  "   \n" +
+  "   // we need to declare an output for the fragment shader\n" +
+  "   out vec4 outColor;\n" +
+  "   \n" +
+  "   void main() {\n" +
+  "     // Just set the output to a constant redish-purple\n" +
+  "     outColor = vec4(1, 0, 0.5, 1);\n" +
+  "   }\n" +
+"";
 
-
-// what goes in this string will be discussed later
-var vertexShaderSource = `#version 300 es
-  // an attribute is an input (in) to a vertex shader.
-  // It will receive data from a buffer
-  in vec4 a_position;
-
-  // all shaders have a main function
-  void main() {
-
-    // gl_Position is a special variable a vertex shader
-    // is responsible for setting
-    gl_Position = a_position;
-  }
-`;
-
-var fragmentShaderSource = `#version 300 es
-  // fragment shaders don't have a default precision so we need
-  // to pick one. mediump is a good default. It means "medium precision"
-  precision mediump float;
-
-  // we need to declare an output for the fragment shader
-  out vec4 outColor;
-
-  void main() {
-    // Just set the output to a constant redish-purple
-    outColor = vec4(1, 0, 0.5, 1);
-  }
-`;
-
+/*
+ * Create and compile a shder
+ * @param gl: WebGL API object
+ * @param type: kinda of shader { vertex, fragment }
+ * @param source: code of the shader
+ * @return shader: a compiled shader
+ */
 function createShader(gl, type, source) {
   var shader = gl.createShader(type);
   gl.shaderSource(shader, source);
@@ -43,11 +47,19 @@ function createShader(gl, type, source) {
     return shader;
   }
 
-  console.log(gl.getShaderInfoLog(shader));  // eslint-disable-line
+  var err_msg = gl.getShaderInfoLog(shader);
+  console.log("webgl: " + err_msg);  // eslint-disable-line
   gl.deleteShader(shader);
-  return undefined;
+  throw Error("Could't compile the shader!\n" + err_msg);
 }
 
+/*
+ * Links the shaders together into a "program"
+ * @param gl: WebGL API object
+ * @param vertexShader: the vertex shader :p
+ * @param fragmnetShader: the fragmnet shader :p
+ * @return program: shader linked
+ */
 function createProgram(gl, vertexShader, fragmentShader) {
   var program = gl.createProgram();
   gl.attachShader(program, vertexShader);
@@ -58,19 +70,22 @@ function createProgram(gl, vertexShader, fragmentShader) {
     return program;
   }
 
-  console.log(gl.getProgramInfoLog(program));  // eslint-disable-line
+  var err_msg = gl.getProgramInfoLog(program);
+  console.log(err_msg);  // eslint-disable-line
   gl.deleteProgram(program);
-  return undefined;
+  throw Error("Could't compile the shader!\n" + err_msg);
 }
 
 function main() {
   // Get A WebGL context
-  // draw over the main canvas from the html
-  var canvas = document.getElementById("main-canvas");
-  // now we interelate the canvas with an object that will be used to draw w/ WebGL
+  var canvas = document.getElementById("webgl-canvas");
+  if (!canvas) {
+    window.alert("Could't find the canvas..");
+    return;
+  }
   var gl = canvas.getContext("webgl2");
   if (!gl) {
-    console.error("Could not get a webgl context");
+    window.alert("Could't get the context..");
     return;
   }
 
@@ -82,78 +97,59 @@ function main() {
   var program = createProgram(gl, vertexShader, fragmentShader);
 
   // look up where the vertex data needs to go.
-  var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+  var posAttrLoc = gl.getAttribLocation(program, "a_position");
 
   // Create a buffer and put three 2d clip space points in it
-  var positionBuffer = gl.createBuffer(); // from now on, every ARRAY_BUFFER operation to the positionBuffer, (think of it as ARRAY_BUFFER = positionBuffer)
+  var bufPos = gl.createBuffer();
 
-  // Bind it to ARRAY_BUFFER (again, think of it as ARRAY_BUFFER = positionBuffer)
-  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = bufPos)
+  gl.bindBuffer(gl.ARRAY_BUFFER, bufPos);
 
-  // triangle cordinates
   var positions = [
-    -0.5, 0.0, 0.0,
-    0.0, 1.0, 0.0,
-    0.5, 0.0, 0.0,
+    0.0, 0.0, 0.0,
+    0.0, 0.5, 0.0,
+    0.7, 0.0, 0.0,
   ];
-
-  // that  is basically a memcopy
-  // except we don't need to say the destination buffer channel (because of the previous bind)
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
-  // context's buffer/origin buffer/context's type of draw (ignore for now)
 
   // Create a vertex array object (attribute state)
-  // that is what describes how data should be read/write to thte buffer itself
   var vao = gl.createVertexArray();
 
   // and make it the one we're currently working with
   gl.bindVertexArray(vao);
-  // that is the description of the data sent to the buffer
-  // after all we just sent a bunch of 0's and 1's
 
   // Turn on the attribute
-  gl.enableVertexAttribArray(positionAttributeLocation);
-  // Q: what is that?
-  // A: otherwise atribute will be a constant value (unespecified)
+  gl.enableVertexAttribArray(posAttrLoc);
 
-  // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
-  // that is basically a struct definition
-  var size      = 3;          // 3 components per iteration
-  var type      = gl.FLOAT;   // the data is 32bit floats
-  var normalize = false;      // don't normalize the data
-  var stride    = 0;          // 0 = move forward size * sizeof(type) each iteration to get the next position
-  var offset    = 0;          // start at the beginning of the buffer
-  gl.vertexAttribPointer(
-      positionAttributeLocation, size, type, normalize, stride, offset);
-  // Q: what is that?
-  // A: the description on how to read data from the attribute
+  // Tell the attribute how to get data out of bufPos (ARRAY_BUFFER)
+  var size = 3;          // 2 components per iteration
+  var type = gl.FLOAT;   // the data is 32bit floats
+  var normalize = false; // don't normalize the data
+  var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
+  var offset = 0;        // start at the beginning of the buffer
+  gl.vertexAttribPointer( posAttrLoc, size, type, normalize, stride, offset );
 
-  // that is only used to accept resising of the window
-  webglUtils.resizeCanvasToDisplaySize(gl.canvas); // that is the function in the webgl helper api
+  webglUtils.resizeCanvasToDisplaySize(gl.canvas);
 
   // Tell WebGL how to convert from clip space to pixels
   gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-  // ocupies the entire canvas
 
   // Clear the canvas
-  gl.clearColor(0, 0, 0, 0); // background is white and transparent, apparently..
-  gl.clear(gl.COLOR_BUFFER_BIT); // Q: what is that?
+  gl.clearColor(0, 0, 0, 0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
 
   // Tell it to use our program (pair of shaders)
   gl.useProgram(program);
-  // tells the engine to use the program with our shaders from before
 
-  // Bind the attribute/buffer set we want.
-  gl.bindVertexArray(vao);
-  // Q: bind the attributes array to where?
+  //  // Bind the attribute/buffer set we want.
+  //  gl.bindVertexArray(vao);
 
-  // draw, only that anything will be actually draw
+  // draw
   var primitiveType = gl.TRIANGLES;
-  var offset        = 0;
-  var count         = 3;
+  var offset = 0;
+  var count = 3;
   gl.drawArrays(primitiveType, offset, count);
-  // draw triangle, from the start of the buffer (offset = 0)
-  // there is COUNT vertexes in the buffer
 }
 
 main();
+
--- webgl.tutorial/webgl.md
+++ webgl.tutorial/webgl.md
@@ -3,7 +3,7 @@
 
 - WebGL is simply a __rasterization engine__ capable of drawing points, line and triangles
   * the part of WebGL that runs on the GPU are the __shaders__
-    + written in GLSM (a C++ like)
+    + written in `GLSM` (a C++ like)
     + there are two shaders: vertex and fragment
 
 - _vertex shader_: compute vertex position
@@ -12,26 +12,26 @@
 - _fragment shader_: computes the color for each pixel
 
 - the API is almost completely about setting up the state of the OpenGL machine
-  * ```gl.drawArrays```` and ```gl.drawElements``` are responsible for calling the shaders
-  * the setting up is bassically about how to provide data to the GPU
+  * `gl.drawArrays` and `gl.drawElements` are responsible for calling the shaders
+  * the setting up is basically about how to provide data to the GPU
 
 ### Receive data in the shaders
 - there are 4 ways shaders can receive data
 
 #### 1. Attributes, Buffers and Vertex Arrays
-- buffers are arrays of binary data you sent to the GPU
+- **BUFFERS** are arrays of binary data you sent to the GPU
   * contains: positions, normals, textures coordinates, vertex color..
 
-- **NOTE**: this data is NOT randomly accessed
+- _NOTE_: this data is NOT randomly accessed
   * for each iteration of the shader it normally access a fixed number
   * of the data in the buffer
 
-- attributes are used to specify __how to pull data from the buffer__
+- **ATTRIBUTES** are used to specify _how to pull data from the buffer_
   * number of parameters
   * size of each parameter (e.g. 32bits floats)
   * start of the buffer, offset
-  * the state of the attributes, which buffer and how use the data is store in the VAO
-    + VAO: vertex array object
+  * the state of the attributes, which buffer and how use the data is store in the `VAO`
+    + **VAO**: vertex array object
 
 #### 2. Uniforms
 - basically global variables set before the execution of the shader
