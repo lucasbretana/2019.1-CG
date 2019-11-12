e1d2385375ddeda3a534896e1d696ab103254ebd
Lucas Eduardo Bretana <lebretana@inf.ufpel.edu.br>
2019-11-12 02:12:34 -0300

    WIP(moving to pixel instead of clipspace)

--- webgl.tutorial/1st.lesson/src/script.js
+++ webgl.tutorial/1st.lesson/src/script.js
@@ -2,33 +2,35 @@
 
 var vertexShaderSource = "" +
   "   #version 300 es \n" +
-  "   // an attribute is an input (in) to a vertex shader.\n" +
-  "   // It will receive data from a buffer\n" +
-  "   in vec4 a_position;\n" +
-  "   \n" +
-  "   // all shaders have a main function\n" +
-  "   void main() {\n" +
-  "   \n" +
-  "     // gl_Position is a special variable a vertex shader\n" +
-  "     // is responsible for setting\n" +
-  "     gl_Position = a_position;\n" +
-  "   }\n" +
+  "   // an attribute is an input (in) to a vertex shader. \n" +
+  "   // It will receive data from a buffer \n" +
+  "   in vec2 a_position; \n" +
+  "   in vec2 u_resolution; \n" +
+  "    \n" +
+  "   // all shaders have a main function \n" +
+  "   void main() { \n" +
+  "     // gl_Position is responsible for setting \n" +
+  "     // fst: go from pixel position to a value between [0, 1] \n" +
+  "     // snd: make this value from: [0, 2] \n" +
+  "     // finally: make this value between [-1, +1] \n" +
+  "     gl_Position = vec4((2.0 * (a_position / u_resolution)) - 1.0, 0.0, 1.0); \n" +
+  "   } \n" +
 "";
 
 var fragmentShaderSource = ""+
-  "   #version 300 es\n" +
+  "   #version 300 es \n" +
   "   \n" +
-  "   // fragment shaders don't have a default precision so we need\n" +
-  "   // to pick one. mediump is a good default. It means \"medium precision\"\n" +
-  "   precision mediump float;\n" +
+  "   // fragment shaders don't have a default precision so we need \n" +
+  "   // to pick one. mediump is a good default. It means \"medium precision\" \n" +
+  "   precision mediump float; \n" +
   "   \n" +
-  "   // we need to declare an output for the fragment shader\n" +
-  "   out vec4 outColor;\n" +
+  "   // we need to declare an output for the fragment shader \n" +
+  "   out vec4 outColor; \n" +
   "   \n" +
-  "   void main() {\n" +
-  "     // Just set the output to a constant redish-purple\n" +
-  "     outColor = vec4(1, 0, 0.5, 1);\n" +
-  "   }\n" +
+  "   void main() { \n" +
+  "     // Just set the output to a constant redish-purple \n" +
+  "     outColor = vec4(1, 0, 0.5, 1); \n" +
+  "   } \n" +
 "";
 
 /*
@@ -98,6 +100,7 @@ function main() {
 
   // look up where the vertex data needs to go.
   var posAttrLoc = gl.getAttribLocation(program, "a_position");
+  var clipUnifLoc = gl.getUniformLocation(program, "u_resolution");
 
   // Create a buffer and put three 2d clip space points in it
   var bufPos = gl.createBuffer();
@@ -106,9 +109,9 @@ function main() {
   gl.bindBuffer(gl.ARRAY_BUFFER, bufPos);
 
   var positions = [
-    0.0, 0.0, 0.0,
-    0.0, 0.5, 0.0,
-    0.7, 0.0, 0.0,
+    10.0, 200.0,
+    80.0, 200.0,
+    10.0, 300.0,
   ];
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
 
@@ -122,7 +125,7 @@ function main() {
   gl.enableVertexAttribArray(posAttrLoc);
 
   // Tell the attribute how to get data out of bufPos (ARRAY_BUFFER)
-  var size = 3;          // 2 components per iteration
+  var size = 2;          // 2 components per iteration
   var type = gl.FLOAT;   // the data is 32bit floats
   var normalize = false; // don't normalize the data
   var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
@@ -141,8 +144,11 @@ function main() {
   // Tell it to use our program (pair of shaders)
   gl.useProgram(program);
 
-  //  // Bind the attribute/buffer set we want.
-  //  gl.bindVertexArray(vao);
+  // 
+  gl.uniform2f(clipUnifLoc, gl.canvas.width, gl.canvas.height);
+
+  // Bind the attribute/buffer set we want.
+  gl.bindVertexArray(vao);
 
   // draw
   var primitiveType = gl.TRIANGLES;
